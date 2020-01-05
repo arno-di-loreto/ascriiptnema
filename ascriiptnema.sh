9b86572899d7d400c90cefd7844f29a88683ff15
arno-di-loreto <arno.di.loreto@gmail.com>
2020-01-05 19:13:52 +0100

    Can be fully stopped using ctlr+c

--- ascriiptnema.sh
+++ ascriiptnema.sh
@@ -1,3 +1,10 @@
+# Trap CTRL+C to be able to exit before the actual end of the script
+trap ctrl_c INT
+function ctrl_c() 
+{
+  exit
+}
+
 script=$1
 prompt=$2
 # Reads the script file line by line
