534136733400c2531e9361b91a894e36b21a56c6
arno-di-loreto <arno.di.loreto@gmail.com>
2020-01-05 19:24:42 +0100

    Printing colored comments without prompts

--- ascriiptnema.sh
+++ ascriiptnema.sh
@@ -1,3 +1,5 @@
+# Example: # ascriiptnema.sh demo-script.sh "\e[36m[colored dummy prompt] $\e[0m"
+
 # Trap CTRL+C to be able to exit before the actual end of the script
 trap ctrl_c INT
 function ctrl_c() 
@@ -10,10 +12,23 @@ prompt=$2
 # Reads the script file line by line
 while IFS= read -r line
 do
-  # Dummy prompt
-  printf "$prompt"
-  # Simulates command typing
-  echo "$line" | pv -qL $[10+(-2 + RANDOM%5)]
-  # Actually executes it
-  eval $line
+  # A comment
+  if [[ $line =~ ^\# ]];
+  then
+    # Lazy cleaning, a comment is supposed do be "# blah blah blah"
+    # So, just removing the first two characters
+    comment=${line:2}
+    # Printing in yellow without prompt
+    # Got (unsolved) problem with coloring when using echo -e, that's why printf is used
+    printf "\e[93m$comment\e[0m" | pv -qL $[10+(-2 + RANDOM%5)]
+    echo
+  # A command line
+  else
+    # Dummy prompt
+    printf "$prompt"
+    # Simulates command typing
+    echo "$line" | pv -qL $[10+(-2 + RANDOM%5)]
+    # Actually executes it
+    eval $line
+  fi
 done < "$script"
--- demo-script.sh
+++ demo-script.sh
@@ -1,6 +1,18 @@
 ls *-openapi.json
+# The \e[0mdemo-api-openapi.json\e[93m file is an OpenAPI file. Let's see what's inside with a good old \e[0mcat\e[93m command
 cat demo-api-openapi.json
+# 
+# 😱 That's unreadable!
+# Let's try to pipe this into jq
 cat demo-api-openapi.json | jq '.'
+# 😍 That's better! 
+# The \e[0mcat demo-api-openapi.json | jq '.'\e[93m command beautified and colored the JSON content 🎉
+# But that's ...
 cat demo-api-openapi.json | jq '.' | wc -l
+# ... lines long. That's still not easy to read in a terminal
+# What if we want to just get the info section?
 cat demo-api-openapi.json | jq '.info'
+# 😎 Cool!
+# And you probaly already guessed how to get only the API name (title in info)
 cat demo-api-openapi.json | jq '.info.title'
+# 🤯 Dead simple isn't it?
