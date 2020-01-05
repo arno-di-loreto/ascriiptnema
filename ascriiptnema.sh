6b0badf3c151d9da38b8244ecf64adf25aa7df46
arno-di-loreto <arno.di.loreto@gmail.com>
2020-01-05 21:19:19 +0100

    Prompt can be changed and revert to default one

--- ascriiptnema.sh
+++ ascriiptnema.sh
@@ -1,4 +1,4 @@
-# Example: # ascriiptnema.sh demo-script.sh "\e[36m[colored dummy prompt] $\e[0m"
+# Example: # ascriiptnema.sh demo-script.sh "\e[36m[default prompt] $\e[0m"
 
 # Trap CTRL+C to be able to exit before the actual end of the script
 trap ctrl_c INT
@@ -8,13 +8,26 @@ function ctrl_c()
 }
 
 script=$1
+defaultprompt=$2
 prompt=$2
 # Reads the script file line by line
+REGEX_PROMPT="^\#[[:blank:]]*PROMPT:[[:blank:]]*"
+REGEX_DEFAULT_PROMPT="^\#[[:blank:]]*DEFAULT_PROMPT"
 while IFS= read -r line
 do
-  # A comment
-  if [[ $line =~ ^\# ]];
+  # Changes prompt
+  if [[ $line =~ $REGEX_PROMPT ]];
   then
+    prompt=`echo $line | sed -e 's/^\#[[:blank:]]*PROMPT:[[:blank:]]*//'`
+    #prompt=`echo $line | sed -e 's/$REGEX_PROMPT//'`
+  # Get back to default prompt
+  elif [[ $line =~ $REGEX_DEFAULT_PROMPT ]];
+  then
+    prompt=$defaultprompt
+  elif [[ $line =~ ^\# ]];
+  then
+    # Dummy prompt
+    printf "$prompt"
     # Lazy cleaning, a comment is supposed do be "# blah blah blah"
     # So, just removing the first two characters
     comment=${line:2}
--- demo-script.sh
+++ demo-script.sh
@@ -1,18 +1,11 @@
-ls *-openapi.json
-# The \e[0mdemo-api-openapi.json\e[93m file is an OpenAPI file. Let's see what's inside with a good old \e[0mcat\e[93m command
-cat demo-api-openapi.json
-# 
-# 😱 That's unreadable!
-# Let's try to pipe this into jq
-cat demo-api-openapi.json | jq '.'
-# 😍 That's better! 
-# The \e[0mcat demo-api-openapi.json | jq '.'\e[93m command beautified and colored the JSON content 🎉
-# But that's ...
-cat demo-api-openapi.json | jq '.' | wc -l
-# ... lines long. That's still not easy to read in a terminal
-# What if we want to just get the info section?
-cat demo-api-openapi.json | jq '.info'
-# 😎 Cool!
-# And you probaly already guessed how to get only the API name (title in info)
-cat demo-api-openapi.json | jq '.info.title'
-# 🤯 Dead simple isn't it?
+ls -l
+# The \e[0mascriiptnema.sh\e[93m script plays any shell script
+cat ascriiptnema.sh
+# 😱 It's working?
+# Let's try to change prompt for the next command
+# PROMPT:  \e[36m[invoking JQ] $\e[0m
+head ascriiptnema.sh
+# 😍 That's even better! 
+# DEFAULT_PROMPT
+# Now we're back to the default prompt
+echo "this is so great  🎉"
